d9eced52ca55a0d715fb476b73f51754917e00d6
AnaCarolinaFreitas <ana.c.freitas35@aluno.senai.br>
2025-04-17 16:14:02 -0300

    Feat: new components

---
+++ app/Feed/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from "./feed.module.css";
+
+export default function feed() {
+    return (
+        <div className={styles.feedContainer}>
+           
+        </div>
+    )
+
+}
