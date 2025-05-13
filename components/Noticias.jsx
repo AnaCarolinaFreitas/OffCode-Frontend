486110699784a5a432814106c72ea4576cfac401
samuelbrag4 <samuelbraga.tds2.senai@gmail.com>
2025-05-13 14:32:40 -0300

    feat: add scrow to news and feed

--- components/Noticias.jsx
+++ components/Noticias.jsx
@@ -2,31 +2,34 @@ import styles from '../styles/Noticias.module.css';
 
 const Noticias = () => {
     const noticias = [
-        {
-            img: '/images/news1.png',
-            texto: 'Tecnologia quântica é inovadora, mas os benefícios ainda são modestos'
-        },
-        {
-            img: '/images/news2.png',
-            texto: 'Avanços em inteligência artificial prometem transformar a indústria'
-        },
-        {
-            img: '/images/news3.png',
-            texto: 'Como se preparar para entrevistas técnicas em grandes empresas'
-        }
-    ];
+    {
+        img: '/tecnologiaQuantica.jpg',
+        texto: 'Tecnologia quântica é inovadora, mas os benefícios ainda são modestos'
+    },
+    {
+        img: '/inteligenciaArtificial.jpg',
+        texto: 'Avanços em inteligência artificial prometem transformar a indústria'
+    },
+    {
+        img: '/entrevistaEmprego.jpg',
+        texto: 'Como se preparar para entrevistas técnicas em grandes empresas'
+    }
+];
 
     return (
-        <div className={styles.sidebar}>
-            <h2>NOTÍCIAS</h2>
+    <div className={styles.sidebar}>
+        <h2>NOTÍCIAS</h2>
+        <div className={styles.newsContainer}>
             {noticias.map((noticia, index) => (
                 <div key={index} className={styles.newsCard}>
-                    <img src={noticia.img} alt="Notícia" />
+                    <img src={noticia.img} alt="Notícia" className={styles.newsImage} />
                     <p>{noticia.texto}</p>
                 </div>
             ))}
         </div>
-    );
+    </div>
+);
+
 };
 
 export default Noticias;
